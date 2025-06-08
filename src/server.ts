1613a8ed547c9f6de70722660b3b1940d99e24ef
Abu shama <mabu53140@gmail.com>
2025-06-08 06:55:00 +0530

    test linta stage 2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 function welcome(name: string) {
-
     console.log('Welcome to the server!' + name)
+
+    const user = {
+        name: 'abu',
+    }
+
+    const fname = user.name
+
+    return name + fname
 }
 
 welcome('John Doe')
